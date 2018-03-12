31a4259898223633dcd70e8364b212b3aebccfcd
hare1039 <hare39393@gmail.com>
2018-03-13 00:55:18 +0800

    Prettify table output. And add password argument

--- 01/nahw1-1_0413220.py
+++ 01/nahw1-1_0413220.py
@@ -16,65 +16,75 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 if __name__ == "__main__":
-    opts, args = getopt.getopt(sys.argv[1:], "h", ["help"])
+    opts, args = getopt.getopt(sys.argv[1:], "hp:", ["help", "password="])
     name = sys.argv[-1]
+    password = ""
     for o, a in opts:
         if o in ("-h", "--help"):
             print("python get_schedule.py [options] student_id")
             print("  [options]")
             print("    -h, --help          -- display this help")
+            print("    -p, --password      -- enter password in arguments")
             sys.exit()
+        if o in ("-p", "--password"):
+            password = a
         else:
             assert False, "Unreconized options"
-    
-    password = getpass.getpass("Password for " + name + ": ")
+    if password == "":
+        password = getpass.getpass("Password for " + name + ": ")
     driver = webdriver.Remote (
         command_executor="http://hare1039.nctu.me:4444/wd/hub",
         desired_capabilities=DesiredCapabilities.CHROME
     )
-
-    driver.get("https://portal.nctu.edu.tw/portal/login.php")
-    parsed = False
-    while not parsed:
-        img = driver.find_element(By.ID, "captcha")
-        img_captcha_base64 = driver.execute_async_script("""
-            var ele = arguments[0], callback = arguments[1];
-            ele.addEventListener("load", function fn() {
-                ele.removeEventListener("load", fn, false);
-                var cnv = document.createElement("canvas");
-                cnv.width = this.width; cnv.height = this.height;
-                cnv.getContext("2d").drawImage(this, 0, 0);
-                callback(cnv.toDataURL("image/jpeg").substring(22));
-            }, false);
-            ele.dispatchEvent(new Event("load"));
-        """, img)
-        with open(r"/tmp/getcaptcha.jpg", "wb") as f:
-            f.write(base64.b64decode(img_captcha_base64))
-        files = {"file": open("/tmp/getcaptcha.jpg", "rb")}
-        res = requests.post("https://hare1039.nctu.me/cracknctu", files=files)
-        os.remove("/tmp/getcaptcha.jpg")
-        if res.status_code == requests.codes.ok and res.text != "ERROR":
-            print(res.text)
-            cap = driver.find_element(By.ID, "seccode")
-            cap.send_keys(res.text)
-            parsed = True
-        else:
-            print("refresh!")
-            reload_but = driver.find_element(By.ID, "seccode_refresh")
-            ActionChains(driver).click(reload_but).perform()
-
-    print("ゲットだぜ")
-
-    namefield = driver.find_element(By.ID, "username")
-    namefield.send_keys(name)
-    passfield = driver.find_element(By.ID, "password")
-    passfield.send_keys(password)
-    passfield.submit()
+    logined = False
+    while not logined:
+        driver.get("https://portal.nctu.edu.tw/portal/login.php")
+        parsed = False
+        while not parsed:
+            img = driver.find_element(By.ID, "captcha")
+            img_captcha_base64 = driver.execute_async_script("""
+                var ele = arguments[0], callback = arguments[1];
+                ele.addEventListener("load", function fn() {
+                    ele.removeEventListener("load", fn, false);
+                    var cnv = document.createElement("canvas");
+                    cnv.width = this.width; cnv.height = this.height;
+                    cnv.getContext("2d").drawImage(this, 0, 0);
+                    callback(cnv.toDataURL("image/jpeg").substring(22));
+                }, false);
+                ele.dispatchEvent(new Event("load"));
+            """, img)
+            with open(r"/tmp/getcaptcha.jpg", "wb") as f:
+                f.write(base64.b64decode(img_captcha_base64))
+            files = {"file": open("/tmp/getcaptcha.jpg", "rb")}
+            res = requests.post("https://hare1039.nctu.me/cracknctu", files=files)
+            os.remove("/tmp/getcaptcha.jpg")
+            if res.status_code == requests.codes.ok and res.text != "ERROR":
+                print(" " + res.text + " ", end="")
+                cap = driver.find_element(By.ID, "seccode")
+                cap.send_keys(res.text)
+                parsed = True
+            else:
+                print(">>", end="", flush=True)
+                reload_but = driver.find_element(By.ID, "seccode_refresh")
+                ActionChains(driver).click(reload_but).perform()
     
+        print("ゲットだぜ")
+    
+        namefield = driver.find_element(By.ID, "username")
+        namefield.send_keys(name)
+        passfield = driver.find_element(By.ID, "password")
+        passfield.send_keys(password)
+        try:
+            passfield.submit()
+            logined = True
+        except selenium.common.exceptions.UnexpectedAlertPresentException:
+            logined = False
+
+            
     driver.get("https://portal.nctu.edu.tw/portal/relay.php?D=cos")
     try:
         element_present = EC.presence_of_element_located((By.ID, "submit"))
-        WebDriverWait(driver, 10).until(element_present)
+        WebDriverWait(driver, 20).until(element_present)
     except TimeoutException:
         print("Timed out waiting for page to load")
         assert False, "Timed out waiting for page to load"
@@ -91,7 +101,7 @@ if __name__ == "__main__":
 
     try:
         element_present = EC.presence_of_element_located((By.XPATH, "/html/body/center/p/table[2]"))
-        WebDriverWait(driver, 10).until(element_present)
+        WebDriverWait(driver, 20).until(element_present)
     except TimeoutException:
         print("Timed out waiting for page to load")
         assert False, "Timed out waiting for page to load"
@@ -103,5 +113,6 @@ if __name__ == "__main__":
     ptable = from_csv(fp)
     fp.close()
     os.remove("/tmp/getclasses.csv")
+    ptable = ptable[1:-1]
     print(ptable)
     driver.quit()
